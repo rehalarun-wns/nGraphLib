a758e369d0286fbaf352d437ea4a6b884b050445
rehalarun-wns <rehalarunstg@gmail.com>
2025-05-24 14:42:58 +0530

    tests added

--- graphs/tests/test_directed_graph.cpp
+++ graphs/tests/test_directed_graph.cpp
@@ -1,9 +1,25 @@
 #include <gtest/gtest.h>
 #include "../directed_graph.h"
 
-TEST(DirectedGraphTest, AddAndCountVerticesAndEdges)
+// Fixture for DirectedGraph<int>
+class DirectedGraphIntTest : public ::testing::Test
 {
+protected:
     DirectedGraph<int> g;
+
+    void SetUp() override
+    {
+        // Optional: common setup for each test
+    }
+
+    void TearDown() override
+    {
+        // Optional: cleanup after each test
+    }
+};
+
+TEST_F(DirectedGraphIntTest, AddAndCountVerticesAndEdges)
+{
     g.AddEdge(1, 2);
     g.AddEdge(2, 3);
 
@@ -11,18 +27,16 @@ TEST(DirectedGraphTest, AddAndCountVerticesAndEdges)
     EXPECT_EQ(g.GetNumOfEdges(), 2);
 }
 
-TEST(DirectedGraphTest, RemoveEdge)
+TEST_F(DirectedGraphIntTest, RemoveEdge)
 {
-    DirectedGraph<int> g;
     g.AddEdge(1, 2);
     g.RemoveEdge(1, 2);
 
     EXPECT_EQ(g.GetNumOfEdges(), 0);
 }
 
-TEST(DirectedGraphTest, RemoveVertex)
+TEST_F(DirectedGraphIntTest, RemoveVertex)
 {
-    DirectedGraph<int> g;
     g.AddEdge(1, 2);
     g.AddEdge(2, 3);
     g.RemoveVertex(2);
@@ -30,3 +44,33 @@ TEST(DirectedGraphTest, RemoveVertex)
     EXPECT_EQ(g.GetNumOfVertices(), 1);
     EXPECT_EQ(g.GetNumOfEdges(), 0);
 }
+
+// Parameterized test for edge addition
+struct EdgeParam
+{
+    int from;
+    int to;
+};
+
+class DirectedGraphParamTest : public ::testing::TestWithParam<EdgeParam>
+{
+};
+
+TEST_P(DirectedGraphParamTest, AddSingleEdge)
+{
+    DirectedGraph<int> g;
+    auto param = GetParam();
+    g.AddEdge(param.from, param.to);
+
+    EXPECT_EQ(g.GetNumOfVertices(), 1);
+    EXPECT_EQ(g.GetNumOfEdges(), 1);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    EdgeCases,
+    DirectedGraphParamTest,
+    ::testing::Values(
+        EdgeParam{1, 2},
+        EdgeParam{2, 3},
+        EdgeParam{-1, 0},
+        EdgeParam{100, 200}));
